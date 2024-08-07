4de3160f09f902469905542492946249bd06cf1b
Talia-Apr <68282150+Talia-Apr@users.noreply.github.com>
2024-08-07 14:21:44 +0700

    final

--- pengguna.js
+++ pengguna.js
@@ -1,26 +1,8 @@
-// // document.addEventListener('DOMContentLoaded', (event) => {
-// //     const navLinks = document.querySelectorAll('.nav-link');
-
-// //     navLinks.forEach(link => {
-// //         link.addEventListener('click', () => {
-// //             navLinks.forEach(nav => nav.classList.remove('active'));
-// //             link.classList.add('active');
-// //         });
-// //     });
-// // });
-
-// let btn = document.querySelector("#btn");
-// let sidebar = document.querySelector("#.sidebar");
-
-// btn.onClick = function(){
-//     sidebar.classList.toggle("active");
-// }
-
 document.addEventListener("DOMContentLoaded", function() {
     const sidebar = document.querySelector(".sidebar");
-    const btn = document.querySelector("#btn");
+    const closeBtn = document.querySelector("#btn");
 
-    btn.addEventListener("click", function() {
+    closeBtn.addEventListener("click", function() {
         sidebar.classList.toggle("active");
     });
 });
